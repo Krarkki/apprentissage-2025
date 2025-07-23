6fd694c7059d381bae22ed5b17c87d34598cb823
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-23 20:55:14 +0200

    ajout de variables String et int dans main.java

--- Java/untitled/src/Main.java
+++ Java/untitled/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello Krarkki");
+    public static void main(String[] args) {
+        int age = 23;
+        String pseudo = "Krarkki";
+        System.out.println( " Hello "+ pseudo + " tu as " + " " + age + "ans");
+
     }
+
 }
