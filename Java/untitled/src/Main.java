5ec0429314228986c461a08865426760d4a77321
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-24 10:28:36 +0200

    ajout de ++ et -- dans main.java

--- Java/untitled/src/Main.java
+++ Java/untitled/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int age = 23;
         String pseudo = "Krarkki";
+        age ++;
         System.out.println( " Hello "+ pseudo + " tu as " + " " + age + "ans");
 
     }
