4c8b0e6db174c4d05239af6938266c3aa12e7709
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-24 10:33:49 +0200

    calcul moyenne de 3 notes dans Main.java

--- Java/untitled/src/Main.java
+++ Java/untitled/src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        int age = 23;
-        String pseudo = "Krarkki";
-        age ++;
-        System.out.println( " Hello "+ pseudo + " tu as " + " " + age + "ans");
-
-    }
-
+public static void main(String[] args) {
+    int note1 = 10;
+    int note2 = 20;
+    int note3 = 14;
+     int calcul = (note1 + note2 + note3) / 3;
+     System.out.println("la moyenne de 10 + 20 + 14 est de " + calcul);
+}
 }
