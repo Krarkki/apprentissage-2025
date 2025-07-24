59b4f959b90213dd13f0645bca9c8bc014c24993
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-24 10:44:14 +0200

    calcul moyenne de 4 notes dans Main.java

--- Java/untitled/src/Main.java
+++ Java/untitled/src/Main.java
@@ -3,7 +3,8 @@ public static void main(String[] args) {
     int note1 = 10;
     int note2 = 20;
     int note3 = 14;
-     int calcul = (note1 + note2 + note3) / 3;
-     System.out.println("la moyenne de 10 + 20 + 14 est de " + calcul);
+    int note4 = 18;
+     int calcul = (note1 + note2 + note3 + note4) / 4;
+     System.out.println("la moyenne de 10 + 20 + 14 + 18 est de " + calcul);
 }
 }
