41de9622ae3dc3b25de608bf8dbcecfe653198fd
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-25 15:48:51 +0200

    création d'une fonction

--- src/ej/Main.java
+++ src/ej/Main.java
@@ -2,8 +2,11 @@ package ej;
 
 public class Main {
     public static void main(String[] args) {
-        String phrase = "Hello World!";
-        System.out.println(phrase);
+        int somme = Main.addition(88,55,48) ;
+        System.out.println(somme) ;
+    }
+    public static int addition(final int entier1, final int entier2, final int entier3) {
+        return entier1 + entier2 + entier3;
     }
 
 
