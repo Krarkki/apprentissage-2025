ec2f5cc18a54b504ebd47d2f32169fc7b9206dcc
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-25 11:56:45 +0200

    nouveau projet ej avec un package

---
+++ src/ej/Main.java
@@ -0,0 +1,14 @@
+package ej;
+
+public class Main {
+    public static void main(String[] args) {
+        int num ;
+        num = 5 ;
+        num = 10 ;
+        final int immuable = 20 ;
+        System.out.println(num + immuable);
+
+
+
+    }
+}
