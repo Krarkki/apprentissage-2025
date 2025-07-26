46424a6a33c01da34a71973234408ecdc6c3dbe2
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-26 11:12:53 +0200

    utilisation de condition if, else if

--- src/ej/Main.java
+++ src/ej/Main.java
@@ -2,12 +2,31 @@ package ej;
 
 public class Main {
     public static void main(String[] args) {
-        affichage("Marty");
+        MajeurOuPas(12);
     }
-    public static void affichage(String texte) {
-        System.out.println("Bonjour " +texte);
+    public static void MajeurOuPas(final int age){
+        if(age>=18) {
+        System.out.println("la personne a " + age + " ans, elle est donc majeure");}
+        else if(age<= 12) {
+        System.out.println("La personne a " + age + " ans, elle est très jeune");}
+        else if(age> 12 && age< 18) {
+        System.out.println("La personne a " + age + " ans, elle est donc une adolescente");}
+
     }
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
 
 
 
