a499af7b5243533cea53f7712f9249a4d411972b
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-27 12:09:55 +0200

    tableau entiers

--- src/ej/Main.java
+++ src/ej/Main.java
@@ -2,7 +2,7 @@ package ej;
 
 public class Main {
     public static void main(String[] args) {
-        int tableau [] = {25,55,66,98,75,48,61};
+        int tableau [] = {25,55,66,98,75,48,62};
 
     }
     }
