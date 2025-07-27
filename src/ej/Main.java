399304f229807678d00c3a86b3cacaacc08a7f0c
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-27 14:26:01 +0200

    blocage tableau entiers avec boucles

--- src/ej/Main.java
+++ src/ej/Main.java
@@ -2,8 +2,13 @@ package ej;
 
 public class Main {
     public static void main(String[] args) {
-        int tableau [] = {25,55,66,98,75,48,62};
+        int tableau [] = {25,55,66,0,75,48,62};
+        int cpt = 0 ;
+        for (int i = tableau.length ; tableau [i] == 0 ; cpt++) {
+            System.out.println(cpt);
 
+
+        }
     }
     }
 
