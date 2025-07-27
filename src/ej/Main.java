372e86de850192c478b0b8f579b2f4a4f97f36db
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-27 12:07:12 +0200

    tableau d'entiers

--- src/ej/Main.java
+++ src/ej/Main.java
@@ -2,27 +2,11 @@ package ej;
 
 public class Main {
     public static void main(String[] args) {
-        MajeurOuPas(12);
-    }
-    public static void MajeurOuPas(final int age){
-        if(age>=18) {
-        System.out.println("la personne a " + age + " ans, elle est donc majeure");}
-        else if(age<= 12) {
-        System.out.println("La personne a " + age + " ans, elle est très jeune");}
-        else if(age> 12 && age< 18) {
-        System.out.println("La personne a " + age + " ans, elle est donc une adolescente");}
+        int tableau [] = {25,55,66,98,75,48,61};
 
     }
+    }
 
-
-
-
-
-
-
-
-
-}
 
 
 
