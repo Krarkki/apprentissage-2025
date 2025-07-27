45ae11c00f684d3d545c1c4382d8313ca794b05d
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-27 21:43:33 +0200

    code final tableau entiers avec boucle qui fonctionne comme je veux

--- src/ej/Main.java
+++ src/ej/Main.java
@@ -2,20 +2,19 @@ package ej;
 
 public class Main {
     public static void main(String[] args) {
-        int tableau [] = {25,0,66,0,75,0,62};
-        int cpt = 0 ;
-        for (int i = 0 ;  i < tableau.length ; i++) {
+        int tableau[] = {25, 0, 66, 0, 75, 0, 62};
+        int cpt = 0;
+        for (int i = 0; i < tableau.length; i++) {
             if (tableau[i] == 0) {
                 cpt++;
-                System.out.println(cpt);}
-
-
-
-
+            }
+        }
+        System.out.println(cpt);
+        {
 
         }
-    }
-    }
+
+    }}
 
 
 
