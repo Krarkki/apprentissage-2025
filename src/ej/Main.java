d0acf071973d68a08e16bf6725c23d6d0d89d1b3
Krarkki <24-01234.enz.kev@isfce.be>
2025-07-28 12:41:25 +0200

    petit push

--- src/ej/Main.java
+++ src/ej/Main.java
@@ -2,7 +2,7 @@ package ej;
 
 public class Main {
     public static void main(String[] args) {
-        int tableau[] = {25, 0, 66, 0, 75, 0, 62};
+        int tableau[] = {25, 0, 66, 0, 75, 0, 62, 88};
         int cpt = 0;
         for (int i = 0; i < tableau.length; i++) {
             if (tableau[i] == 0) {
